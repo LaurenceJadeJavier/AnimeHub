399ad4520affd75e35f9d359376cb0f87084706d
unknown <helagabler@gmail.com>
2024-04-27 13:50:09 +0800

    props fixed

--- customizedStyle/CustomizedGrid.tsx
+++ customizedStyle/CustomizedGrid.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CustomizedGrid = ({children, classname}:any) => {
+const CustomizedGrid = ({children, className}:any) => {
   return (
-    <div className={`p-5 grid grid-cols-5 bg-slate-500 `}>
+    <div className={`p-5 grid grid-cols-5 bg-slate-500 ${className}`}>
         {children}
     </div>
   )
